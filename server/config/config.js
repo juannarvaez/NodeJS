fcd140576f948b49f0bb4699493899ae0d8fca97
JuanC <juan.narvaez@correounivalle.edu.co>
2018-10-05 11:25:10 -0500

    Variable de entorno para conexion a DB MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -9,10 +9,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // Bases de datos
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe'
-// } else {
-urlDB = 'mongodb://admin:cafe-admin7@ds223763.mlab.com:23763/cafe'
-    // }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = process.env.MONGO_URI
+}
 
 process.env.URLDB = urlDB;
